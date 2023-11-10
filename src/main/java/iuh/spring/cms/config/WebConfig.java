e61967678226413bbd65bc826fe6d66fec0c1035
duongCute123 <89206817+duongCute123@users.noreply.github.com>
2023-11-10 15:20:10 +0700

    nice

---
+++ src/main/java/iuh/spring/cms/config/WebConfig.java
@@ -0,0 +1,16 @@
+package iuh.spring.cms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://ophim9.cc")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
