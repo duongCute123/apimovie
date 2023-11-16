fb6a38d083b17dd14679146ce354d3c75150f60b
duongCute123 <89206817+duongCute123@users.noreply.github.com>
2023-11-16 13:48:34 +0700

    not fix proxy ophim9.cc

--- src/main/java/iuh/spring/cms/config/AppConfig.java
+++ src/main/java/iuh/spring/cms/config/AppConfig.java
@@ -1,21 +1,13 @@
 package iuh.spring.cms.config;
 
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
 	@Bean
 	public RestTemplate restTemplate() {
-		SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-		Proxy proxy = new Proxy(Proxy.Type.HTTP,
-				new InetSocketAddress("https://apimovie-production-2565.up.railway.app/", 3006));
-		requestFactory.setProxy(proxy);
-		return new RestTemplate(requestFactory);
+		return new RestTemplate();
 	}
 }
