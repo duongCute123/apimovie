cfefc2d084d76cc21eb72d006379ef1477cf47af
duongCute123 <89206817+duongCute123@users.noreply.github.com>
2023-11-16 11:07:11 +0700

    fix

--- src/main/java/iuh/spring/cms/config/AppConfig.java
+++ src/main/java/iuh/spring/cms/config/AppConfig.java
@@ -1,13 +1,21 @@
 package iuh.spring.cms.config;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
 	@Bean
 	public RestTemplate restTemplate() {
-		return new RestTemplate();
+		SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+		Proxy proxy = new Proxy(Proxy.Type.HTTP,
+				new InetSocketAddress("https://apimovie-production-2565.up.railway.app/", 3006));
+		requestFactory.setProxy(proxy);
+		return new RestTemplate(requestFactory);
 	}
 }
